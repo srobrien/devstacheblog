d07c8ad240bca6c967bc9938ab226340bc0e78b1
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-08-21 21:17:32 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Gatsby Default Starter",
+    prefixPath: "/devstacheblog",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-filesystem',
-      options: { name: 'src', path: `${__dirname}/src` },
+      resolve: "gatsby-source-filesystem",
+      options: { name: "src", path: `${__dirname}/src` },
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: { name: 'img', path: `${__dirname}/src/images` },
+      resolve: "gatsby-source-filesystem",
+      options: { name: "img", path: `${__dirname}/src/images` },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         excerpt_separator: `<!--end-->`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
   ],
-}
+};
