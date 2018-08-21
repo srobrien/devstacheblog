22201049c8804133b41fae24bde2c0ff77989eae
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-08-21 21:23:26 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby Default Starter",
   },
-  prefixPath: "/devstacheblog",
+  pathPrefix: "/devstacheblog",
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
