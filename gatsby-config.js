6b8302b7364342b80e4e5b13d17b0cf8fce67c24
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-08-22 07:36:18 +0100

    Config File Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby Default Starter",
   },
-  pathPrefix: "/devstacheblog",
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
