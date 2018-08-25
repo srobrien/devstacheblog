86d37b6d8ee8cad75cea42fc2e26c63521a22c7e
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-08-25 14:07:13 +0100

    added position sticky to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -42,6 +42,10 @@ const HeaderWrapper = styled.div`
   background-color: #e583e2;
   background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239e7fa1' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
   clip-path: polygon(0 0, 100% 0, 100% 45%, 0 100%);
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  z-index: 1000;
   @media (max-width: 600px) {
     clip-path: none;
     grid-template-rows: 20px repeat(2, 1fr);
