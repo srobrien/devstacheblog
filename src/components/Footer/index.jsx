5e1c1d9916c8056a68df42dcefcc4e2f90b3a261
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-09-17 08:21:43 +0100

    footer updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,13 @@ import selfie from "../../images/superhandsomedude.png";
 
 const FooterBar = styled.div`
   @media (max-width: 690px) {
-    min-height: 500px;
+    min-height: 460px;
   }
+
+  @media (max-width: 600px) {
+    clip-path: none;
+  }
+
   position: relative;
   background-color: #70c8c2;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 80 80' width='80' height='80'%3E%3Cpath fill='%23e6e3ec' fill-opacity='0.4' d='M14 16H9v-2h5V9.87a4 4 0 1 1 2 0V14h5v2h-5v15.95A10 10 0 0 0 23.66 27l-3.46-2 8.2-2.2-2.9 5a12 12 0 0 1-21 0l-2.89-5 8.2 2.2-3.47 2A10 10 0 0 0 14 31.95V16zm40 40h-5v-2h5v-4.13a4 4 0 1 1 2 0V54h5v2h-5v15.95A10 10 0 0 0 63.66 67l-3.47-2 8.2-2.2-2.88 5a12 12 0 0 1-21.02 0l-2.88-5 8.2 2.2-3.47 2A10 10 0 0 0 54 71.95V56zm-39 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm40-40a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM15 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm40 40a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'%3E%3C/path%3E%3C/svg%3E");
@@ -19,6 +24,7 @@ const HandsomeDude = styled.div`
     flex-direction: column-reverse;
     justify-items: right;
   }
+
   display: flex;
   width: 600px;
   position: absolute;
@@ -47,6 +53,7 @@ const TextArea = styled.div`
   @media (max-width: 690px) {
     text-align: right;
     .up {
+      margin-top: 10px;
       display: inline-block;
     }
     .side {
@@ -99,16 +106,18 @@ export default class Footer extends Component {
             <p>
               CURLED UP IN THE FETAL POSITION ON THE FLOOR WONDERING, WHERE THE
             </p>
-            <p>HELL DID 30 YEARS OF MYLIFE GO?! ROSS GET A GRIP OF YOURSELF </p>
-            <br />
             <p>
-              MAN! MAKE A CHANGE GODDAMN IT! DO SOMETHING WITH YOUR LIFE!
-              SOMETHING
+              HELL DID 30 YEARS OF MYLIFE GO?! ROSS GET A GRIP OF YOURSELF MAN!
+              MAKE A
             </p>
             <br />
             <p>
-              YOU ENJOY! SOMETHING THAT MAKES YOU WANT TO GET OUT OF BED EVERY
-              MORNING!
+              CHANGE GODDAMN IT! DO SOMETHING WITH YOUR LIFE! SOMETHING YOU
+              ENJOY!
+            </p>
+            <br />
+            <p>
+              SOMETHING THAT MAKES YOU WANT TO GET OUT OF BED EVERY MORNING!
             </p>
             <br />
             <p>FAILING THAT, TRAIN TO BECOME A DEV...</p> <br />
