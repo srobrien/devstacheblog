4bce5efe9ada5909031513d1056112103ecf9410
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-09-23 20:05:50 +0100

    test image sizes

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,10 +31,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images-contentful`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 400,
+              maxWidth: 600,
             },
           },
         ],
