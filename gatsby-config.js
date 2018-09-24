cbfec37d73fc6377c96d73699ab9d6879e75e2f3
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-09-24 09:42:31 +0100

    more anayltics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       options: {
         trackingId: "UA-126280941-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
