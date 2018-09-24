f87d4b16c531c84977ee5557f86e2a8abaac1e78
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2018-09-24 09:50:59 +0100

    more anayltics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,17 +29,11 @@ module.exports = {
         // Puts tracking script in the head instead of the body
         head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        anonymize: false,
         // Setting this parameter is also optional
-        respectDNT: true,
+        respectDNT: false,
         // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "UA-126280941-1",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "www.devstache.io",
       },
     },
 
